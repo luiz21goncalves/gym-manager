30cea5aa5d09ee88a928fdcf42e37b5e128a11b3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-17 08:47:08 -0300

    first commit

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.get("/", function(req, res) {
+  return res.redirect("/instructors");
+});
+
+routes.get("/instructors", function(req, res) {
+  return res.render("instructors/index");
+});
+
+routes.get("/membres", function(req, res) {
+  return res.render("membres");
+});
+
+module.exports = routes;
