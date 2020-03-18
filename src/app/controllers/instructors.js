586fda599c88029955f98c1c2fd8146e4662f7c1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-18 14:40:04 -0300

    refatorando  gym-manager

---
+++ src/app/controllers/instructors.js
@@ -0,0 +1,41 @@
+const { age, date } = require("../../lib/utils")
+
+module.exports = {
+  index(req, res) {
+    return res.render("instructors/index");
+  },
+  create(req, res) {
+    return res.render("instructors/create");
+  },
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Por favor preencha todos os campos")
+      }
+    }
+  
+    return
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Por favor preencha todos os campos")
+      }
+    }
+
+    return
+  },
+  delete(req, res) {
+    return
+  },
+}
