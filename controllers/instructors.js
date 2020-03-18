7e3bc7f137f016c4f0921d01f24bb59a1e84dec3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-18 10:04:50 -0300

    funcionalidades de instrutores finalidada

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const data = require("../data.json")
 const { age, date } = require("../utils")
 
+exports.index = function(req, res) {
+  return res.render("instructors/index", { instructors: data.instructors });
+}
+
 exports.show = function(req, res) {
   const { id } = req.params
 
@@ -69,4 +73,49 @@ exports.edit = function(req, res) {
   }
 
   return res.render("instructors/edit", { instructor });
+};
+
+exports.put = function(req, res) {
+  const { id } = req.body
+  let index = 0;
+
+  const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
+    if (id == instructor.id) {
+      index = foundIndex;
+      return true;
+    }
+  });
+
+  if (!foundInstructor) return res.send("Instrutor não encontrado");
+
+  const instructor = {
+    ...foundInstructor,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id),
+  };
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Erro ao salvar o arquivo")
+
+    return res.redirect(`/instructors/${id}`);
+  });
+};
+
+exports.delete = function(req, res) {
+  const { id } =  req.body;
+
+  const filteredInstructors = data.instructors.filter(function(instructor) {
+    return id != instructor.id;
+  });
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Erro ao salvar o arquivo")
+
+    return res.redirect("/instructors");
+  });
 };
